4352399f3c84160321239b6df7cffd2e7763e20b
elto <404088770@qq.com>
2017-03-23 11:24:23 +0800

    计算深度

--- src/com/topic/company/TreeNodeDepth.java
+++ src/com/topic/company/TreeNodeDepth.java
@@ -1,1 +1,31 @@
+package com.topic.company;
+import java.util.Scanner;
+
+
+public class TreeNodeDepth {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String treeStr = null;
+		String queryNodes = null;
+		treeStr = sc.nextLine();
+		queryNodes = sc.nextLine();
+		sc.close();
+		
+		System.out.println(treeStr);
+		System.out.println(queryNodes);
+		
+		String[] nodes = queryNodes.split("");
+		int maxDepth = treeStr.charAt(treeStr.length() - 1);
+		int[] nodeDepth = new int[nodes.length];
+		
+		for(int i=0; i < nodes.length; i++) {
+			if(treeStr.indexOf(nodes[i]) > -1) {
+				nodeDepth[i] = maxDepth + 1 - treeStr.charAt(treeStr.indexOf(nodes[i]) + 1);
+			}
+		}
+		for(int i = 0; i < nodes.length; i++) {
+			System.out.print(nodeDepth[i] + " ");
+		}
+	}
+}
 
